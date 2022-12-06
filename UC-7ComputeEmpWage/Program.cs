8a74d9ce517ebe9c29181cd7f873e9d5a2b3e8fa
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-06 15:25:54 +0530

    Adding UC-7EmpWageComp

---
+++ UC-7ComputeEmpWage/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace computeEmpWage
+{
+    internal class Program
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 2;
+        public const int MAX_HOURS_IN_MONTH = 10;
+
+        public static int computeEmpWage()
+        {
+            int empHrs = 0, totalempHrs = 0, totalWorkingDays = 0;
+            while (totalempHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+
+                }
+                totalempHrs += empHrs;
+                Console.WriteLine("Days:" + totalWorkingDays + "Emp Hrs:" + empHrs);
+            }
+            int totalEmpWage = totalempHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Emp Wage; " + totalEmpWage);
+            return totalEmpWage;
+        }
+        static void Main(string[] args)
+        {
+            computeEmpWage();
+        }
+    }  }
