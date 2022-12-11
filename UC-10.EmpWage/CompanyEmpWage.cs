183944a46f7636df0cb1f851adf6e76e3df3cc3b
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-11 16:01:27 +0530

    EmpWage using Interface approach

---
+++ UC-10.EmpWage/CompanyEmpWage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC_10.EmpWage
+{
+    public class CompanyEmpWage
+    {
+        public string company;
+        public int empRatePerHours;
+        public int numOfWorkingDays;
+        public int maxHoursPerMonth;
+        public int totalEmpWage;
+
+        public CompanyEmpWage(string company, int empRatePerHours, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHours = empRatePerHours;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+        }
+        public string toString()
+        {
+            return "Total Emp Wage for company : " + this.company + " is : " + this.totalEmpWage;
+        }
+    }
+}
---
+++ UC-10.EmpWage/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UC_10.EmpWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+        }
+    }
+}
